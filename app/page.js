75eec55b241bea1ff7af337dc7bbc1eb138067b7
KDN-hub <kamsiyochinwakwuribe@gmail.com>
2026-05-27 23:08:36 +0100

    Add float animation to avatar

--- app/page.js
+++ app/page.js
@@ -33,15 +33,17 @@ export default function Home() {
             </div>
           </AnimatedSection>
           
-          <AnimatedSection direction="left" delay={200} className="mt-12 lg:mt-0 h-72 w-72 lg:h-96 lg:w-96 rounded-full overflow-hidden flex items-center justify-center relative flex-shrink-0 shadow-2xl border-4 border-dark-tertiary">
-            <Image
-              src="/profile.jpg"
-              alt="Profile Picture"
-              width={600}
-              height={600}
-              className="h-full w-full object-cover object-top"
-              priority
-            />
+          <AnimatedSection direction="left" delay={200}>
+            <div className="animate-float mt-12 lg:mt-0 h-72 w-72 lg:h-96 lg:w-96 rounded-full overflow-hidden flex items-center justify-center relative flex-shrink-0 shadow-2xl border-4 border-dark-tertiary">
+              <Image
+                src="/profile.jpg"
+                alt="Profile Picture"
+                width={600}
+                height={600}
+                className="h-full w-full object-cover object-top"
+                priority
+              />
+            </div>
           </AnimatedSection>
         </div>
       </section>
